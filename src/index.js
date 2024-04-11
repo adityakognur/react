637ca8b246b0b14e22545133316d08ebbb9bc6b0
Aditya <118843539+adityakognur@users.noreply.github.com>
2024-04-11 22:05:00 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+var name='aditya'
+num=7;
+ReactDOM.render(
+<div>
+    <h1>Hello {name}!</h1>
+    <p>lucky number {num}</p>
+</div>
+, document.getElementById("root"));
